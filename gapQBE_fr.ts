2704b6657e8ff38f5d56ee7ae87573b3c305a131
gthom <gthomassingap@gmail.com>
2010-01-20 18:29:32 +0100

    ajout des fichiers de traduction

---
+++ gapQBE_fr.ts
@@ -0,0 +1,415 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fr_FR" sourcelanguage="en">
+<defaultcodec>UTF-8</defaultcodec>
+<context>
+    <name>DialogLogin</name>
+    <message>
+        <location filename="dialoglogin.ui" line="14"/>
+        <source>GapTableConceptor-login</source>
+        <translation>GapTableConceptor-Connexion</translation>
+    </message>
+    <message>
+        <location filename="dialoglogin.ui" line="22"/>
+        <source>Login:</source>
+        <translation>Identifiant:</translation>
+    </message>
+    <message>
+        <location filename="dialoglogin.ui" line="29"/>
+        <source>root</source>
+        <translation>root</translation>
+    </message>
+    <message>
+        <location filename="dialoglogin.ui" line="40"/>
+        <source>Password:</source>
+        <translation>Mot de Passe:</translation>
+    </message>
+    <message>
+        <location filename="dialoglogin.ui" line="61"/>
+        <source>Database Name:</source>
+        <translation>Nom de la Base</translation>
+    </message>
+    <message>
+        <location filename="dialoglogin.ui" line="68"/>
+        <source>mysql</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="dialoglogin.ui" line="79"/>
+        <source>Host:</source>
+        <translation>Serveur:</translation>
+    </message>
+    <message>
+        <location filename="dialoglogin.ui" line="93"/>
+        <source>Database Engine:</source>
+        <translation>Type de base:</translation>
+    </message>
+    <message>
+        <location filename="dialoglogin.ui" line="120"/>
+        <source>&amp;Ok</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="dialoglogin.ui" line="127"/>
+        <source>&amp;Cancel</source>
+        <translation>&amp;Annuler</translation>
+    </message>
+</context>
+<context>
+    <name>DialogTypeJointure</name>
+    <message>
+        <location filename="dialogtypejointure.ui" line="17"/>
+        <source>Type de jointure</source>
+        <translation>Type de jointure</translation>
+    </message>
+    <message>
+        <location filename="dialogtypejointure.ui" line="25"/>
+        <source>Please choose a type:</source>
+        <translation>Type de jointure:</translation>
+    </message>
+    <message>
+        <location filename="dialogtypejointure.ui" line="33"/>
+        <source>Natural</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="dialogtypejointure.ui" line="38"/>
+        <source>Inner</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="dialogtypejointure.ui" line="43"/>
+        <source>left outer</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="dialogtypejointure.ui" line="48"/>
+        <source>right outer</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>QCustomGraphicsScene</name>
+    <message>
+        <location filename="qcustomgraphicsscene.cpp" line="52"/>
+        <source>Add a &amp;free field</source>
+        <translation>ajout d&apos;un champ &amp;libre</translation>
+    </message>
+    <message>
+        <location filename="qcustomgraphicsscene.cpp" line="53"/>
+        <source>Add an aggregate</source>
+        <translation>Ajouter une fonction agrégat</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="lien.cpp" line="230"/>
+        <location filename="table.cpp" line="75"/>
+        <source>&amp;Remove</source>
+        <translation>&amp;Supprimer</translation>
+    </message>
+    <message>
+        <location filename="lien.cpp" line="231"/>
+        <source>&amp;Change join type</source>
+        <translation>&amp;Changer le type de jointure</translation>
+    </message>
+    <message>
+        <location filename="lien.cpp" line="236"/>
+        <source>&amp;Edit join condition</source>
+        <translation>&amp;Editer la condition de jointure</translation>
+    </message>
+    <message>
+        <location filename="lien.cpp" line="254"/>
+        <source>Edit Join Condition</source>
+        <translation>Edition de la condition de jointure</translation>
+    </message>
+    <message>
+        <location filename="lien.cpp" line="255"/>
+        <source>Join Condition:</source>
+        <translation>Condition:</translation>
+    </message>
+    <message>
+        <location filename="table.cpp" line="77"/>
+        <source>&amp;Add free Field</source>
+        <translation>&amp;Ajouter un champ libre</translation>
+    </message>
+    <message>
+        <location filename="table.cpp" line="80"/>
+        <source>&amp;Remove alias</source>
+        <translation>&amp;Retirer l&apos;alias</translation>
+    </message>
+    <message>
+        <location filename="table.cpp" line="82"/>
+        <source>&amp;Add alias</source>
+        <translation>&amp;Donner un alias</translation>
+    </message>
+    <message>
+        <location filename="table.cpp" line="132"/>
+        <source>Give alias name</source>
+        <translation>Donner un alias</translation>
+    </message>
+    <message>
+        <location filename="table.cpp" line="133"/>
+        <source>Alias:</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>dialogRelation</name>
+    <message>
+        <location filename="dialogrelation.cpp" line="589"/>
+        <source>Do you really want to Quit</source>
+        <translation>Voulez-vous réellement quitter l&apos;application?</translation>
+    </message>
+    <message>
+        <location filename="dialogrelation.cpp" line="630"/>
+        <source>Define sort order</source>
+        <translation>Définition de l&apos;ordre de tri</translation>
+    </message>
+    <message>
+        <location filename="dialogrelation.ui" line="14"/>
+        <source>Gap Query Builder QBE</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="dialogrelation.ui" line="40"/>
+        <source>Table&apos;s list from database</source>
+        <translation>Liste des tables de la base</translation>
+    </message>
+    <message>
+        <location filename="dialogrelation.ui" line="43"/>
+        <source>choose tables you need</source>
+        <translation>Sélectionnez les tables concernées</translation>
+    </message>
+    <message>
+        <location filename="dialogrelation.ui" line="46"/>
+        <source>Tables you need</source>
+        <translation>Tables dont vous avez besoin</translation>
+    </message>
+    <message>
+        <location filename="dialogrelation.ui" line="88"/>
+        <source>oin tables</source>
+        <translation>Relier les tables</translation>
+    </message>
+    <message>
+        <location filename="dialogrelation.ui" line="91"/>
+        <location filename="dialogrelation.ui" line="105"/>
+        <location filename="dialogrelation.ui" line="166"/>
+        <source>...</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="dialogrelation.ui" line="102"/>
+        <source>move tables</source>
+        <translation>déplacer les tables</translation>
+    </message>
+    <message>
+        <location filename="dialogrelation.ui" line="116"/>
+        <source>automatic update of result</source>
+        <translation>mise à jour automatique du résultat</translation>
+    </message>
+    <message>
+        <location filename="dialogrelation.ui" line="136"/>
+        <source>execute  query</source>
+        <translation>Executer la requête</translation>
+    </message>
+    <message>
+        <location filename="dialogrelation.ui" line="150"/>
+        <source>addselected  tables to work space</source>
+        <translation>Ajoute les tables sélectionnées à la requête</translation>
+    </message>
+    <message>
+        <location filename="dialogrelation.ui" line="163"/>
+        <source>best fit in view</source>
+        <translation>adapte à la fenêtre</translation>
+    </message>
+    <message>
+        <location filename="dialogrelation.ui" line="177"/>
+        <source>define result&apos;s sort order </source>
+        <translation>définir l&apos;ordre de tri</translation>
+    </message>
+    <message>
+        <location filename="dialogrelation.ui" line="180"/>
+        <source>SO</source>
+        <translation>OT</translation>
+    </message>
+    <message>
+        <location filename="dialogrelation.ui" line="191"/>
+        <source>remove result&apos;s doubles</source>
+        <translation>retire les doublons</translation>
+    </message>
+    <message>
+        <location filename="dialogrelation.ui" line="194"/>
+        <source>D</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="dialogrelation.ui" line="228"/>
+        <source>Workspace: tables you need</source>
+        <translation>Espace de travail, les tables dont vous avez besoin</translation>
+    </message>
+    <message>
+        <location filename="dialogrelation.ui" line="243"/>
+        <source>SQL:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="dialogrelation.ui" line="250"/>
+        <source>the sql query</source>
+        <translation>la requête sql générée</translation>
+    </message>
+    <message>
+        <location filename="dialogrelation.ui" line="257"/>
+        <source>?</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="dialogrelation.ui" line="274"/>
+        <source>The query&apos;s rows </source>
+        <translation>Le résultat de la requête</translation>
+    </message>
+    <message>
+        <location filename="dialogrelation.ui" line="283"/>
+        <source>Group By</source>
+        <translation>Groupement</translation>
+    </message>
+    <message>
+        <location filename="dialogrelation.ui" line="292"/>
+        <source>Aggregate:</source>
+        <translation>Fonctions de groupe:</translation>
+    </message>
+    <message>
+        <location filename="dialogrelation.ui" line="302"/>
+        <source>&amp;Add</source>
+        <translation>&amp;Ajouter</translation>
+    </message>
+    <message>
+        <location filename="dialogrelation.ui" line="320"/>
+        <source>Postcondition</source>
+        <translation>Condition sur le résultat d&apos;une fonction de groupe</translation>
+    </message>
+    <message>
+        <location filename="dialogrelation.ui" line="351"/>
+        <source>&amp;Quit</source>
+        <translation>&amp;Quitter</translation>
+    </message>
+    <message>
+        <location filename="dialogrelation.ui" line="366"/>
+        <source>suprime</source>
+        <translation>Supprime</translation>
+    </message>
+    <message>
+        <location filename="dialogrelation.ui" line="369"/>
+        <source>supprimer le champ</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="dialogrelation.ui" line="372"/>
+        <source>Del</source>
+        <translation>Effacer</translation>
+    </message>
+    <message>
+        <location filename="dialogrelation.ui" line="377"/>
+        <source>actionSuprTable</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="dialogrelation.ui" line="382"/>
+        <source>zoom in</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="dialogrelation.ui" line="385"/>
+        <source>Ctrl++</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="dialogrelation.ui" line="393"/>
+        <location filename="dialogrelation.ui" line="396"/>
+        <source>zoom out</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="dialogrelation.ui" line="399"/>
+        <source>Ctrl+-</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>dialogSortOrder</name>
+    <message>
+        <location filename="dialogsortorder.ui" line="14"/>
+        <source>Dialog</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="dialogsortorder.ui" line="43"/>
+        <source>Up</source>
+        <translation>Monter</translation>
+    </message>
+    <message>
+        <location filename="dialogsortorder.ui" line="56"/>
+        <source>Down</source>
+        <translation>Descendre</translation>
+    </message>
+    <message>
+        <location filename="dialogsortorder.ui" line="98"/>
+        <source>&amp;Cancel</source>
+        <translation>&amp;Annuler</translation>
+    </message>
+    <message>
+        <location filename="dialogsortorder.ui" line="111"/>
+        <source>&amp;Ok</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>field</name>
+    <message>
+        <location filename="field.cpp" line="40"/>
+        <source>&amp;Remove</source>
+        <translation>&amp;Supprimer</translation>
+    </message>
+    <message>
+        <location filename="field.cpp" line="42"/>
+        <source>&amp;Display(on/off)</source>
+        <translation>&amp;Afficher(on/off)</translation>
+    </message>
+    <message>
+        <location filename="field.cpp" line="48"/>
+        <source>&amp;AddCondition</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="field.cpp" line="52"/>
+        <source>&amp;removeCondition</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="field.cpp" line="55"/>
+        <source>Sort(asc,desc,off)</source>
+        <translation>Tri(croissant,décroissant pas de tri)</translation>
+    </message>
+    <message>
+        <location filename="field.cpp" line="71"/>
+        <source>&amp;Asc</source>
+        <translation>&amp;Croissant</translation>
+    </message>
+    <message>
+        <location filename="field.cpp" line="72"/>
+        <source>&amp;Desc</source>
+        <translation>&amp;Décroissant</translation>
+    </message>
+    <message>
+        <location filename="field.cpp" line="73"/>
+        <source>&amp;Off</source>
+        <translation>&amp;Pas de tri</translation>
+    </message>
+    <message>
+        <location filename="field.cpp" line="111"/>
+        <source>=&quot;something&quot;</source>
+        <translation>=&quot;valeur&quot;</translation>
+    </message>
+</context>
+</TS>
